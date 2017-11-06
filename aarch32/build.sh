43f19c9fbe08e65dc6144795c3bff129f3653b32
Antonio Niño Díaz <antonio_nd@outlook.com>
2017-11-06 08:48:02 +0000

    aarch32: Add initial AArch32 bootstrap
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

---
+++ aarch32/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -x
+
+${CROSS_COMPILE_AARCH32}gcc -c -o el2-bootstrap.o el2-bootstrap.S -march=armv8-a
+${CROSS_COMPILE_AARCH32}ld -Bstatic --gc-sections -nostartfiles -nostdlib -o el2-bootstrap.elf -T el2-bootstrap.ld.S el2-bootstrap.o
+${CROSS_COMPILE_AARCH32}objcopy -O binary el2-bootstrap.elf el2-bootstrap.bin
